9fd77ccea47a143bf7fa45b2e00ad93dbe1e41ac
Niv Oridocs <fradige95@gmail.com>
2024-10-05 21:36:39 +0200

    Add missing annotation

--- src/main/java/niv/burning/impl/DynamicField.java
+++ src/main/java/niv/burning/impl/DynamicField.java
@@ -4,8 +4,11 @@ import java.lang.reflect.Field;
 import java.util.Optional;
 import java.util.function.Function;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import com.google.common.collect.ImmutableMap;
 
+@ApiStatus.Internal
 public abstract class DynamicField {
 
     private static final ImmutableMap<Class<?>, Function<Field, ? extends DynamicField>> MAP;
